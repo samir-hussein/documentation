76c6c5770a50aa5426d387cfcba51426ba4113b9
samir-hussein <62563657+samir-hussein@users.noreply.github.com>
2022-04-19 00:58:46 +0200

    Update routes.js

--- asset/js/routes.js
+++ asset/js/routes.js
@@ -3,7 +3,7 @@ var myApp = angular.module('myApp',['ngRoute']);
 myApp.config(function($routeProvider, $locationProvider) {
 	$routeProvider
 	.when("/",{
-		templateUrl : "/pages/home.html",
+		templateUrl : "https://samir-hussein.github.io/documentation/pages/home.html",
 	})
     .otherwise('/');
 
